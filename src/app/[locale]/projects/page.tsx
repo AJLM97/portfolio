1cbb9832fe3140206b6a63c9335eb931f3cf77ae
AJLM97 <alejandrojavierlemi@gmail.com>
2023-11-17 13:23:18 -0300

    projects grid fixed

--- src/app/[locale]/projects/page.tsx
+++ src/app/[locale]/projects/page.tsx
@@ -1,12 +1,12 @@
 import { useLocale } from 'next-intl';
+import projects from "@/projects.json";
 import Project from '@/components/project';
-import projects from "@/projects.json"
 
 export default function Projects() {
     const locale = useLocale();
     return (
         <div className="grid grid-cols-3 lg:grid-cols-5 auto-rows-auto gap-4">
-            {projects.toReversed().map((item) => {
+            {[...projects].reverse().map((item) => {
                 return <Project key={item.name} src={`${item.name}.png`} alt={item.name} text={item.description[locale == "en" ? "en" : "es"]} link={item.github} technologies={item.technologies} />
             })}
         </div>
